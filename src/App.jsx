8da701dbed52895107e65a99a64e45b1b410ff9b
José Francisco <frakra1978@gmail.com>
2025-05-13 15:03:25 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+
+import './App.css'
+
+function App() {
+   
+  return (
+    <>
+       <header>
+        <h1>José Francisco</h1>
+        <p>Técnico em informática - instrutor - apaixonado or tecnologia</p>
+      </header>
+        <main>
+        <h2>sobre mim</h2>
+        <p>Lorem ipsum dolor sit amet<stong>consectetur</stong> adipisicing elit. Saepe obcaecati mollitia 
+          cum sit ad animi, assumenda cupiditate quasi nemo nihil eligendi dolorum culpa nam 
+          deleniti non debitis praesentium reiciendis repellat.</p>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa doloremque deserunt at esse corporis
+             a atque voluptatem aperiam quos reiciendis, nihil consequuntur impedit natus. Beatae enim quasi earum. 
+             Autem, laudantium.</p>
+             <h3>Habilidades principais</h3>
+              <ul>
+                <li>Montagem e configuraçãode PCs</li>
+                <li>Redes e conectividades</li>
+                <li>Sistemas Operacionaist</li>
+                <li>Didática técnica para cursos e certificações</li>
+              </ul>
+        </main>
+        <footer>
+          &copy; 2023 José Francisco - Todos os direitos reservados
+        </footer>
+        
+    </>
+  )
+}
+
+export default App
